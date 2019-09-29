59b3077d8bc65c777a37ff673446ec5dc387f876
Louis Arthur <louisartur.53@gmail.com>
2019-09-29 13:39:02 -0300

    start the little project

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // presets adicionados pelo yarn e que utilizarei, o primeiro ele altera as funcionalidades do JS que o
+  //navegador não entende.
+  // o segundo vai transformar as coisas que o navegador não entende na parte do react
+  presets: ["@babel/preset-env", "@babel/preset-react"]
+};
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+module.exports = {
+  //o arquivo de entrada, usando o path, pois no windows pode dar problemas
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public")
+  },
+  // regras, o babel irá pegar os arquivos javascripts, o bebel deverar transpilar
+  //os arquivos js
+  module: {
+    rules: [
+      {
+        //expressão regular.
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
